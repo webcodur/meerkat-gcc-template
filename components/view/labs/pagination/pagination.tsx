e19330776d8262272bac1e7e4f6ec63d675003d1
webcodur <webcodur@gmail.com>
2025-01-03 11:28:19 +0900

    chore(language): add translation

--- components/view/labs/pagination/pagination.tsx
+++ components/view/labs/pagination/pagination.tsx
@@ -1,28 +1,35 @@
-import React, { memo } from 'react'
-import { PaginatedTableDark, PaginatedTableLight } from '@/components/ui/paginatedTable/PaginatedTable'
+'use client';
+import React, { memo } from 'react';
+import { useTranslations } from 'next-intl';
+import {
+    PaginatedTableDark,
+    PaginatedTableLight,
+} from '@/components/ui/paginatedTable/PaginatedTable';
 
 // 테이블 컴포넌트를 메모이제이션
-const MemoizedDarkTable = memo(PaginatedTableDark)
-const MemoizedLightTable = memo(PaginatedTableLight)
+const MemoizedDarkTable = memo(PaginatedTableDark);
+const MemoizedLightTable = memo(PaginatedTableLight);
 
 const PaginationPage = () => {
-  return (
-    <div className="space-y-8 pt-8">
-      <div>
-        <h2 className="text-2xl font-bold mb-4">다크 모드</h2>
-        <div className="bg-gray-900 rounded-xl p-4">
-          <MemoizedDarkTable />
-        </div>
-      </div>
-      
-      <div>
-        <h2 className="text-2xl font-bold mb-4">라이트 모드</h2>
-        <div className="bg-gray-300 rounded-xl p-4">
-          <MemoizedLightTable />
-        </div>
-      </div>
-    </div>
-  )
-}
+    const t = useTranslations('Pagination');
 
-export default memo(PaginationPage)
+    return (
+        <div className="space-y-8 pt-8">
+            <div>
+                <h2 className="text-2xl font-bold mb-4">{t('darkMode')}</h2>
+                <div className="bg-gray-900 rounded-xl p-4">
+                    <MemoizedDarkTable />
+                </div>
+            </div>
+
+            <div>
+                <h2 className="text-2xl font-bold mb-4">{t('lightMode')}</h2>
+                <div className="bg-gray-300 rounded-xl p-4">
+                    <MemoizedLightTable />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default memo(PaginationPage);
