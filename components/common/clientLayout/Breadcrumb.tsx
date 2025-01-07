aaf375c147bbfa8758461f238443b33e4278a8ec
webcodur <webcodur@gmail.com>
2025-01-07 10:08:25 +0900

    feat(UI) Breadcrumb 컴포넌트 실제 값 사용

--- components/common/clientLayout/Breadcrumb.tsx
+++ components/common/clientLayout/Breadcrumb.tsx
@@ -2,33 +2,50 @@ import Link from 'next/link';
 import { IoHome } from 'react-icons/io5';
 import { useAtom } from 'jotai';
 import { sidebarOpenAtom } from '@/atoms';
+import { usePathname } from 'next/navigation';
+import { getMenuItems } from '@/data/constants/sidebarMenus';
+import { useTranslations } from 'next-intl';
 
 export default function Breadcrumb() {
-
     const [sidebarOpen] = useAtom(sidebarOpenAtom);
+    const pathnameWithoutLocale = (usePathname() || '/').slice(3);
+
     const msClass = sidebarOpen 
         ? 'ms-1 transition-[margin] duration-400 ease-in-out' 
         : 'ms-7 transition-[margin] duration-400 ease-in';
 
+    const t = useTranslations();
+    const menuItems = getMenuItems(t);
+
+    const currentMenu = menuItems.find(menu => 
+        menu.path && pathnameWithoutLocale.startsWith(menu.path)
+    );
+    const currentSubMenu = currentMenu?.subMenus?.find(subMenu => 
+        pathnameWithoutLocale === subMenu.path
+    );
+
     return (
-        <div className={`text-md breadcrumbs mt-4 font-medium ${msClass}`}
-        >
+        <div className={`text-md breadcrumbs mt-4 font-medium ${msClass}`}>
             <ul>
                 <li>
                     <Link href="/" className="flex items-center gap-1">
                         <IoHome className="w-4 h-4 " />   
                     </Link>
                 </li>
-                <li>
-                    <p className="flex items-center gap-1">
-                        메뉴
-                    </p>
-                </li>
-                <li>
-                    <Link href="/" className="flex items-center gap-1">
-                        서브메뉴
-                    </Link>
-                </li>
+                {currentMenu && (
+                    <li>
+                        <span className="flex items-center gap-1">
+                            {currentMenu.title}
+                        </span>
+                    </li>
+                )}
+                {currentSubMenu && (
+                    <li>
+                        <span className="flex items-center gap-1">
+                            {currentSubMenu.title}
+                        </span>
+                    </li>
+                )}
             </ul>
         </div>
     );
