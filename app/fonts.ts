27d4e2879eb37eb030a3917591ae01d41496dc42
webcodur <webcodur@gmail.com>
2025-01-10 21:31:16 +0900

    fix(UI): 폰트 파일 로컬로 처리

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,23 +1,22 @@
-import { DM_Serif_Text, Cairo } from 'next/font/google';
 import localFont from 'next/font/local';
 
 // Pretendard
 export const pretendard = localFont({
-  src: '../assets/fonts/PretendardVariable.woff2',
+  src: '../assets/fonts/Pretendard.woff2',
   variable: '--font-ko',
   display: 'swap',
 });
 
 // DM Serif Text
-export const dmSerifText = DM_Serif_Text({
-  weight: ['400'],
-  display: 'swap',
+export const dmSerifText = localFont({
+  src: '../assets/fonts/DMSerifText.woff2',
   variable: '--font-en',
+  display: 'swap',
 });
 
 // Cairo
-export const cairo = Cairo({
-  weight: ['400', '500', '700'],
-  display: 'swap',
+export const cairo = localFont({
+  src: '../assets/fonts/Cairo.woff2',
   variable: '--font-ar',
+  display: 'swap',
 });
